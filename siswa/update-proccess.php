e2764041a8a25f525ab00918054da7f180f6a3a5
fillahaufi <fillahaufi140101@gmail.com>
2021-12-23 11:33:02 +0700

    error solved

--- siswa/update-proccess.php
+++ siswa/update-proccess.php
@@ -17,31 +17,36 @@ if (isset($_POST['update'])) {
     $id = $_SESSION['id'];
     // var_dump($password);
 
-    $query1 = mysqli_query($db, "UPDATE admin SET nama_admin = '$nama_admin', email = '$email' WHERE id = $id");
-    $query2 = mysqli_query($db, "UPDATE userdata SET nohp = '$nohp', alamat = '$alamat' WHERE id_siswa = $id");
-
-    if ($query1 && $query2) {
-        if (isset($password) && isset($password2)) {
-            if ($password !== $password2) {
-                echo "
+    if (isset($password) && isset($password2)) {
+        if ($password !== $password2) {
+            echo "
                 <script>
                     alert('Password tidak sesuai')
                 </script>
             ";
-                header('Location: profile.php?status=gagal');
-                // return false;
-            } else {
-                // var_dump($_SESSION['id']);
-                mysqli_query($db, "UPDATE admin SET password = '$password' WHERE id = $id");
-                header('Location: profile.php?status=success');
-            }
+            header('Location: profile.php?status=gagal');
+            // return false;
         } else {
+            // var_dump($_SESSION['id']);
+            $query1 = mysqli_query($db, "UPDATE admin SET nama_admin = '$nama_admin', email = '$email' WHERE id = $id");
+            $query2 = mysqli_query($db, "UPDATE userdata SET nohp = '$nohp', alamat = '$alamat' WHERE id_siswa = $id");
 
+            if ($query1 && $query2) {
+                $_SESSION['nama'] = $nama_admin;
+                $_SESSION['login_in'] = $email;
+            } else {
+                header('Location: profile.php?status=gagal');
+            }
+
+            mysqli_query($db, "UPDATE admin SET password = '$password' WHERE id = $id");
             header('Location: profile.php?status=success');
         }
-    } else {
-        header('Location: profile.php?status=gagal');
     }
+    // if ($query1 && $query2) {
+
+    // } else {
+    //     header('Location: profile.php?status=gagal');
+    // }
 } else {
     die("Akses dilarang...");
 }
